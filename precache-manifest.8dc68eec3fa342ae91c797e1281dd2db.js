5c0d5583f42a8bb0d90298c7e9c20273b7eafd77
Reiko Reinup <reiko@Reikos-MacBook-Pro.local>
2020-03-06 05:13:36 +0200

    test

---
+++ precache-manifest.8dc68eec3fa342ae91c797e1281dd2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ae1b1ee59eb2c8eb60555debbed90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91be846c188639de916",
+    "url": "/static/css/main.8b336c9e.chunk.css"
+  },
+  {
+    "revision": "1a7cf88f86228c60dc29",
+    "url": "/static/js/2.e4fc89de.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.e4fc89de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91be846c188639de916",
+    "url": "/static/js/main.ef9d11b4.chunk.js"
+  },
+  {
+    "revision": "1688aede09df145c3f1c",
+    "url": "/static/js/runtime-main.021fa1e0.js"
+  }
+]);
